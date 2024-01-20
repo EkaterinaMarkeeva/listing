09088d1633742f2e2166bbf08d1ce69047cfffdf
Ekaterina Markeeva <lesekaterina25@gmail.com>
2024-01-20 20:00:06 +0400

    Corrected Item item-price and App fixed the problem of typing data

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import { Listing } from "./components/listing/Listing";
 import etsy from "./data/etsy.json";
 import { ItemType } from './types/ItemType';
 
-const data: ItemType[] = etsy.filter(etsy => etsy.state !== 'removed');
+const data: ItemType[] = etsy
+  .filter(item => item.state !== 'removed')
+  .map(item => ({
+    ...item,
+    url: item.url || 'default-url',
+    title:  item.title || 'default-title',
+    currency_code: item.currency_code || 'default-currency_code',
+    price: item.price || '0',
+    quantity: item.quantity || 0
+  }));
 
 function App() {
   return (
--- src/components/listing/Item.tsx
+++ src/components/listing/Item.tsx
@@ -13,7 +13,7 @@ export const Item: FC<ItemProps> = ({data}) => {
     </div>
     <div className="item-details">
       <p className="item-title">{data.title.length > 50 ? data.title.slice(0, 50) + '...': data.title}</p>
-      <p className="item-price">{data.currency_code}{data.price}</p>
+      <p className="item-price">{data.currency_code === 'USD' ? '$': (data.currency_code === 'EUR' ? '€': data.currency_code + ' ')}{data.price}</p>
       <p className={`item-quantity ${data.quantity <= 10 ? 'level-low': (data.quantity <= 20 ? 'level-medium': 'level-high')}`}>{data.quantity} left</p>
     </div>
   </div>
